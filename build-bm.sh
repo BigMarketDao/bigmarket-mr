9f788e40ce01b0faa786a76645f728dc2dcb974a
mike <mjoecohen@gmail.com>
2025-01-17 14:33:51 +0000

    target environment when building

--- build-bm.sh
+++ build-bm.sh
@@ -20,7 +20,7 @@ docker push mijoco/bigmarket_api:latest
 
 #docker pull mijoco/bigmarket_api;
 docker rm -f bigmarket_api  
-docker run -d -t -i --network host --env-file ~/.env --name $DOCKER_NAME -p 6070:6070 $DOCKER_NAME
+docker run -d -t -i --network host -e NODE_ENV=testnet --env-file ~/.env --name $DOCKER_NAME -p 6070:6070 $DOCKER_NAME
 docker logs -f $DOCKER_NAME
 
 printf "Finished....\n"
