0aac2a93de7212c39ec4211a95b35d154fae945e
mike <mjoecohen@gmail.com>
2025-07-21 11:04:40 +0100

    Implement new contract events

--- src/routes/dao/events/processors/process_scalar_market_events.ts
+++ src/routes/dao/events/processors/process_scalar_market_events.ts
@@ -11,6 +11,7 @@ import {
 	updateDisputeResolutionEvent,
 	updateMarketStakeEvent,
 	updatePredictionMarketCreateEvent,
+	updatePriceBandWidth,
 	updateResolveMarketEvent,
 	updateResolveMarketUndisputedEvent,
 	updateResolveMarketVoteEvent,
