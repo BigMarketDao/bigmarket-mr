e09b5b82225c1e4ddaa825a270405bc064bcfab2
mike <mjoecohen@gmail.com>
2025-07-21 11:26:41 +0100

    Implement new contract events

--- src/routes/predictions/markets_helper.ts
+++ src/routes/predictions/markets_helper.ts
@@ -191,7 +191,7 @@ export async function updatePriceBandWidth(marketType: number, result: any, basi
 	// (print {event: "price-band-width", feed-id: feed-id, precent: band-bips})
 	const resolveEvent = {
 		...basicEvent,
-		feedId: Number(result.value['feed-id']?.value || 'unknown'),
+		feedId: result.value['feed-id']?.value || 'unknown',
 		bandBips: Number(result.value.precent?.value || 0)
 	};
 	await saveOrUpdateEvent(resolveEvent);
