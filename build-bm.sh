d215e59a3d69b447b77e3876e30171f3e2274ff7
mike <mjoecohen@gmail.com>
2025-09-25 10:29:45 +0200

    parameterises the startup

--- build-bm.sh
+++ build-bm.sh
@@ -1,28 +1,46 @@
 #!/bin/bash -e
 #
-############################################################
+# Usage: ./deploy.sh testnet
+#        ./deploy.sh mainnet
 
-SERVER=spinoza.brightblock.org;
-DOCKER_NAME=bigmarket_api
+NETWORK=$1
+if [ -z "$NETWORK" ]; then
+  echo "Usage: $0 [testnet|mainnet]"
+  exit 1
+fi
 
+SERVER=spinoza.brightblock.org
+DOCKER_NAME="bigmarket_api_${NETWORK}"
 
 printf "\n\n"
 printf "====================================================\n"
 printf "Building on: $SERVER as docker container: $DOCKER_NAME \n"
 
-source ~/.profile;
-#cd ~/hubgit/bigmarket/bigmarket-api
-#git pull https://github.com/radicleart/bigmarket-api.git daoless
-docker login;
-docker build --build-arg NODE_ENV=testnet -t mijoco/bigmarket_api:latest .
-docker push mijoco/bigmarket_api:latest
-docker rm -f bigmarket_api
-docker run -d -t -i --network host -e NODE_ENV=testnet --env-file ~/.env --name $DOCKER_NAME -p 6090:6090 mijoco/bigmarket_api:latest
+source ~/.profile
+
+docker login
+docker build --build-arg NODE_ENV=$NETWORK -t mijoco/bigmarket_api:$NETWORK .
+docker push mijoco/bigmarket_api:$NETWORK
+
+docker rm -f $DOCKER_NAME || true
+
+# Use different ports if needed (e.g. 6090 for testnet, 6091 for mainnet)
+PORT="6090"
+if [ "$NETWORK" = "mainnet" ]; then
+  PORT="6091"
+fi
+
+docker run -d -t -i \
+  --network host \
+  -e NODE_ENV=$NETWORK \
+  --env-file ~/.env \
+  --name $DOCKER_NAME \
+  -p $PORT:6090 \
+  mijoco/bigmarket_api:$NETWORK
+
 docker logs -f $DOCKER_NAME
 
 printf "Finished....\n"
 printf "====================================================\n\n"
 
-exit 0;
-
-
+exit 0
